6824cadabcde13f8eccd5bd4cba61f7e16f99329
david tiago <davidtiago27@gmail.com>
2021-09-10 00:42:40 -0500

    Agregando estilos al contador

--- proyecto-react-david/src/components/ItemListContainer/ItemCount.js
+++ proyecto-react-david/src/components/ItemListContainer/ItemCount.js
@@ -1,1 +1,53 @@
-import React from 'react'
+import Button from 'react-bootstrap/Button';
+import {useState} from 'react'
+import Card from 'react-bootstrap/Card';
+
+
+
+const ItemCount = ({stock, initial, onAdd}) => {
+    const [count, setCount] = useState(initial)
+
+    function Suma () {
+        if (count<stock){
+            setCount(count +1 )
+        }
+        
+    }
+
+    function resta (){
+        if (count>1){
+            setCount(count -1 )
+        }
+        
+    }
+
+    const agregarBolsa=()=>{
+        onAdd(count)
+    }
+
+    return (
+        
+        <div>
+            
+            {/* <label>{count}</label><br/>
+            <Button variant="dark" onClick={Suma}>+</Button>
+            <Button variant="secondary" onClick={agregarBolsa}>Agregar a la bolsa</Button>
+            <Button variant="dark" onClick={resta}>-</Button> */}
+            <Card style={{ width: '18rem' }}>            
+            <Card.Body>
+                <Card.Title>Bolsa</Card.Title>
+                <Card.Text>{count}</Card.Text>
+                <Button variant="dark" onClick={Suma}>+</Button>
+                <Button variant="secondary" onClick={agregarBolsa}>Agregar a la bolsa</Button>
+                <Button variant="dark" onClick={resta}>-</Button>
+            </Card.Body>
+            </Card>
+
+
+        </div> 
+        
+    )
+
+}
+
+export default ItemCount
--- proyecto-react-david/src/components/ItemListContainer/ItemListContainer.js
+++ proyecto-react-david/src/components/ItemListContainer/ItemListContainer.js
@@ -1,9 +1,18 @@
 import React from 'react'
+import ItemCount from './ItemCount'
 
 function ItemListContainer ({bienvenida}) {
+
+    const onAdd = (cant) => {
+        // console.log(cant)
+        alert('¡Agregaste ' + cant + ' libros a tu bolsa!')
+    }
+
     return (
+        
         <div>
-            {bienvenida}
+            <h2>{bienvenida}</h2>
+            <ItemCount stock={10} initial={1} onAdd={onAdd}/>
         </div>
     )
 }

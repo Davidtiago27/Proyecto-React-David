3b303d2b7bd56441441c83dc6c21471121a3c952
david tiago <davidtiago27@gmail.com>
2021-09-09 23:28:32 -0500

    Agreando ItemListContainer

--- proyecto-react-david/src/App.js
+++ proyecto-react-david/src/App.js
@@ -1,11 +1,17 @@
 import './App.css';
 import Navbar from './components/Navbar/Navbar';
 import 'bootstrap/dist/css/bootstrap.min.css';
+import ItemListContainer from './components/ItemListContainer/ItemListContainer';
 
 
 function App() {
   return (
+    <>  
+
     <Navbar />
+    <ItemListContainer bienvenida='Hola, bienvenido a tu libreria favorita' /> 
+
+    </>
   )
   
 }
---
+++ proyecto-react-david/src/components/ItemListContainer/ItemListContainer.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+function ItemListContainer ({bienvenida}) {
+    return (
+        <div>
+            {bienvenida}
+        </div>
+    )
+}
+
+export default ItemListContainer
